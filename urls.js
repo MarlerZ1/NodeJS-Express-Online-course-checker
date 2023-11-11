0ad1d0d1db1e4312087aceb3af148466b97b898e
MarlerZ1 <kirill4421@yandex.ru>
2023-11-11 11:24:12 +0300

    Rework db interaction, add second page

--- urls.js
+++ urls.js
@@ -1,14 +1,31 @@
-function setUrls(app){
+const mysql = require('mysql2')
 
-    app.get('/', (req, res) => {
-        res.render('teachers/groupList', {
-            'groups': [],
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '11337799Ee__',
+    database: 'new_db'
+})
+connection.connect()
+
+function setUrls(app){
+    connection.query("SELECT * FROM studentgroup;", (err, result) =>{
+        app.get('/', (req, res) => {
+            res.render('teachers/groupList', {
+                'groups':  result,
+            });
+        });
+    });
+    app.get('/user/group/:id', (req, res) => {
+        console.log(req.params.id)
+        connection.query("SELECT * FROM user WHERE groupId=?;", [req.params.id], (err, result) =>{
+                res.render('teachers/group', {
+                    'students':  result,
+                });
+
         });
     });
 
-    app.get('/user/:username/:id', (req, res) => {
-        res.send('This is page' + req.params.id)
-    })
 
 }
 
